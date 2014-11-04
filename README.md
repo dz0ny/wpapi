4e1dcfabd343918764a392950ad08ebdb67a53f4
dz0ny <dz0ny@ubuntu.si>
2014-11-04 22:07:06 +0100

    Enable caching

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
 # Deploy
-
 1. heroku create -b https://github.com/kr/heroku-buildpack-go.git
 2. git push
+
+
+# Develop
+1. wget https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz; tar -xvf godeb-amd64.tar.gz; ./godeb install 1.3.3
+2. https://github.com/Masterminds/glide#install
+3. glide in
+
+
+# Testing
+1. glide in
+2. go test
+
+
+# API
+
+- Latest URL for theme ```http://wpapi.herokuapp.com/theme/editor/zip```
+- Latest URL for direct theme download ```http://wpapi.herokuapp.com/theme/editor/download```
+- Latest URL for plugin ```http://wpapi.herokuapp.com/plugin/akismet/zip```
+- Latest URL for direct plugin download ```http://wpapi.herokuapp.com/plugin/akismet/download```
+
+
+# Benchmark
+```
+boom -n 1000 -c 50 -q 59 http://wpapi.herokuapp.com/theme/editor/zip
+1000 / 1000 Boooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo! 100.00 % 
+
+Summary:
+  Total:	17.0869 secs.
+  Slowest:	1.1467 secs.
+  Fastest:	0.1292 secs.
+  Average:	0.1707 secs.
+  Requests/sec:	58.5244
+
+Status code distribution:
+  [200]	994 responses
+  [429]	6 responses
+
+Response time histogram:
+  0.129 [1]	|
+  0.231 [947]	|∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎∎
+  0.333 [6]	|
+  0.434 [7]	|
+  0.536 [8]	|
+  0.638 [7]	|
+  0.740 [6]	|
+  0.841 [7]	|
+  0.943 [7]	|
+  1.045 [3]	|
+  1.147 [1]	|
+
+Latency distribution:
+  10% in 0.1372 secs.
+  25% in 0.1405 secs.
+  50% in 0.1450 secs.
+  75% in 0.1511 secs.
+  90% in 0.1620 secs.
+  95% in 0.2762 secs.
+  99% in 0.8618 secs.
+```
--- main.go
+++ main.go
@@ -5,53 +5,62 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/gorelic"
+	"github.com/martini-contrib/gzip"
 	"github.com/martini-contrib/throttle"
 	"github.com/stretchr/hoard"
 	"log"
+	"net/http"
 	"os"
 	"time"
 )
 
 var m *martini.Martini
 
-func GetTheme(name string) string {
+func GetTheme(name string) (string, bool) {
 	url := fmt.Sprintf("https://wordpress.org/themes/%s", name)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
-		log.Fatal(err)
+		return "", false
 	}
-	href, _ := doc.Find(".col-3 .button a").First().Attr("href")
-	log.Println(href)
-	return href
+	href, found := doc.Find(".col-3 .button a").First().Attr("href")
+	return href, found
+}
+
+func GetPlugin(name string) (string, bool) {
+	url := fmt.Sprintf("https://wordpress.org/plugins/%s", name)
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		return "", false
+	}
+	href, found := doc.Find("#plugin-description .button a").First().Attr("href")
+	return href, found
 }
 
 func GetThemeCached(name string) string {
 	return hoard.Get(fmt.Sprintf("GetTheme_%s", name), func() (interface{}, *hoard.Expiration) {
-		obj := GetTheme(name)
-		return obj, hoard.Expires().AfterMinutes(30)
+		url, found := GetTheme(name)
+		if found {
+			return url, hoard.Expires().AfterMinutes(5)
+		} else {
+			return "", hoard.Expires().AfterMinutes(30)
+		}
 	}).(string)
-}
-
-func GetPlugin(name string) string {
-	url := fmt.Sprintf("https://wordpress.org/plugins/%s", name)
-	doc, err := goquery.NewDocument(url)
-	if err != nil {
-		log.Fatal(err)
-	}
-	href, _ := doc.Find("#plugin-description .button a").First().Attr("href")
-	log.Println(href)
-	return href
 }
 
 func GetPluginCached(name string) string {
 	return hoard.Get(fmt.Sprintf("GetPlugin_%s", name), func() (interface{}, *hoard.Expiration) {
-		obj := GetTheme(name)
-		return obj, hoard.Expires().AfterMinutes(30)
+		url, found := GetPlugin(name)
+		if found {
+			return url, hoard.Expires().AfterMinutes(5)
+		} else {
+			return "", hoard.Expires().AfterMinutes(30)
+		}
 	}).(string)
 }
 
 func main() {
 	m := martini.Classic()
+	m.Use(gzip.All())
 	limits := throttle.Policy(&throttle.Quota{
 		Limit:  60,
 		Within: time.Second,
@@ -63,11 +72,46 @@ func main() {
 	}
 
 	m.Use(limits)
-	m.Get("/theme/:name/zip", func(params martini.Params) string {
-		return GetTheme(params["name"])
+	m.Get("/theme/:name/zip", func(params martini.Params) (int, string) {
+		url := GetThemeCached(params["name"])
+		log.Println(url)
+		if url != "" {
+			return http.StatusOK, url
+		} else {
+			return http.StatusNotFound, "Theme was not found"
+		}
 	})
-	m.Get("/plugin/:name/zip", func(params martini.Params) string {
-		return GetPlugin(params["name"])
+	m.Get("/plugin/:name/zip", func(params martini.Params) (int, string) {
+		url := GetPluginCached(params["name"])
+		log.Println(url)
+		if url != "" {
+			return http.StatusOK, url
+		} else {
+			return http.StatusNotFound, "Plugin was not found"
+		}
+	})
+
+	m.Get("/theme/:name/download", func(r *http.Request, w http.ResponseWriter, params martini.Params) {
+		url := GetThemeCached(params["name"])
+		log.Println(url)
+		if url != "" {
+			http.Redirect(w, r, fmt.Sprintf("https:%s", url), 302)
+			return
+		} else {
+			http.NotFound(w, r)
+			return
+		}
+	})
+	m.Get("/plugin/:name/download", func(r *http.Request, w http.ResponseWriter, params martini.Params) {
+		url := GetPluginCached(params["name"])
+		log.Println(url)
+		if url != "" {
+			http.Redirect(w, r, url, 302)
+			return
+		} else {
+			http.NotFound(w, r)
+			return
+		}
 	})
 
 	m.Run()
