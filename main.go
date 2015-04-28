ac9cda7d9584ed195d3c0a3d274fa75f0681f6d0
dz0ny <dz0ny@ubuntu.si>
2015-04-28 12:47:46 +0200

    Switch to gin

--- main.go
+++ main.go
@@ -2,22 +2,17 @@ package main
 
 import (
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/gorelic"
-	"github.com/martini-contrib/gzip"
-	"github.com/martini-contrib/throttle"
-	"github.com/stretchr/hoard"
-	"log"
 	"net/http"
 	"os"
 	"strings"
-	"time"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/gin-gonic/contrib/newrelic"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/hoard"
 )
 
-var m *martini.Martini
-
-func GetTheme(name string) (string, bool) {
+func getTheme(name string) (string, bool) {
 	url := fmt.Sprintf("https://wordpress.org/themes/%s", name)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
@@ -27,7 +22,7 @@ func GetTheme(name string) (string, bool) {
 	return href, found
 }
 
-func GetPlugin(name string) (string, bool) {
+func getPlugin(name string) (string, bool) {
 	url := fmt.Sprintf("https://wordpress.org/plugins/%s", name)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
@@ -37,7 +32,7 @@ func GetPlugin(name string) (string, bool) {
 	return href, found
 }
 
-func GetThemeThumbnail(name string) (string, bool) {
+func getThemeThumbnail(name string) (string, bool) {
 	url := fmt.Sprintf("https://wordpress.org/themes/%s", name)
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
@@ -50,9 +45,9 @@ func GetThemeThumbnail(name string) (string, bool) {
 	return href, found
 }
 
-func GetThemeCached(name string) string {
+func getThemeCached(name string) string {
 	return hoard.Get(fmt.Sprintf("GetTheme_%s", name), func() (interface{}, *hoard.Expiration) {
-		url, found := GetTheme(name)
+		url, found := getTheme(name)
 		if found {
 			return url, hoard.Expires().AfterMinutes(5)
 		} else {
@@ -61,9 +56,9 @@ func GetThemeCached(name string) string {
 	}).(string)
 }
 
-func GetThemeThumbnailCached(name string) string {
+func getThemeThumbnailCached(name string) string {
 	return hoard.Get(fmt.Sprintf("GetThemeThumbnail_%s", name), func() (interface{}, *hoard.Expiration) {
-		url, found := GetThemeThumbnail(name)
+		url, found := getThemeThumbnail(name)
 		if found {
 			return url, hoard.Expires().AfterMinutes(5)
 		} else {
@@ -72,83 +67,68 @@ func GetThemeThumbnailCached(name string) string {
 	}).(string)
 }
 
-func GetPluginCached(name string) string {
+func getPluginCached(name string) string {
 	return hoard.Get(fmt.Sprintf("GetPlugin_%s", name), func() (interface{}, *hoard.Expiration) {
-		url, found := GetPlugin(name)
+		url, found := getPlugin(name)
 		if found {
 			return url, hoard.Expires().AfterMinutes(5)
 		} else {
 			return "", hoard.Expires().AfterMinutes(30)
 		}
 	}).(string)
+}
+
+func themeCached(c *gin.Context) {
+	name := c.Params.ByName("name")
+	url := getThemeCached(name)
+	if url != "" {
+		c.Redirect(http.StatusMovedPermanently, url)
+	} else {
+		c.String(http.StatusNotFound, "Theme was not found")
+	}
+}
+
+func pluginCached(c *gin.Context) {
+	name := c.Params.ByName("name")
+	url := getPluginCached(name)
+	if url != "" {
+		c.Redirect(http.StatusMovedPermanently, url)
+	} else {
+		c.String(http.StatusNotFound, "Plugin was not found")
+	}
+}
+
+func thumbCached(c *gin.Context) {
+	name := c.Params.ByName("name")
+	url := getThemeThumbnailCached(name)
+	if url != "" {
+		c.Redirect(http.StatusMovedPermanently, url)
+	} else {
+		c.String(http.StatusNotFound, "Theme was not found")
+	}
+}
+
+func GetMainEngine() *gin.Engine {
+	r := gin.Default()
+	nr := os.Getenv("NEWRELIC")
+	if nr != "" {
+		r.Use(newrelic.NewRelic(nr, "wpapi", false))
+	}
+
+	r.HEAD("/theme/:name/download", themeCached)
+	r.GET("/theme/:name/download", themeCached)
+	r.HEAD("/plugin/:name/download", pluginCached)
+	r.GET("/plugin/:name/download", pluginCached)
+	r.GET("/theme/:name/thumbnail", thumbCached)
+	return r
 }
 
 func main() {
-	m := martini.Classic()
-	m.Use(gzip.All())
-	limits := throttle.Policy(&throttle.Quota{
-		Limit:  60,
-		Within: time.Second,
-	})
-	nr := os.Getenv("NEWRELIC")
-	if nr != "" {
-		m.Use(gorelic.Handler)
-		gorelic.InitNewrelicAgent(nr, "wpapi", true)
+	r := GetMainEngine()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
 	}
-
-	m.Use(limits)
-	m.Get("/theme/:name/zip", func(params martini.Params) (int, string) {
-		url := GetThemeCached(params["name"])
-		log.Println(url)
-		if url != "" {
-			return http.StatusOK, url
-		} else {
-			return http.StatusNotFound, "Theme was not found"
-		}
-	})
-	m.Get("/plugin/:name/zip", func(params martini.Params) (int, string) {
-		url := GetPluginCached(params["name"])
-		log.Println(url)
-		if url != "" {
-			return http.StatusOK, url
-		} else {
-			return http.StatusNotFound, "Plugin was not found"
-		}
-	})
-
-	m.Get("/theme/:name/download", func(r *http.Request, w http.ResponseWriter, params martini.Params) {
-		url := GetThemeCached(params["name"])
-		log.Println(url)
-		if url != "" {
-			http.Redirect(w, r, fmt.Sprintf("https:%s", url), 302)
-			return
-		} else {
-			http.NotFound(w, r)
-			return
-		}
-	})
-	m.Get("/plugin/:name/download", func(r *http.Request, w http.ResponseWriter, params martini.Params) {
-		url := GetPluginCached(params["name"])
-		log.Println(url)
-		if url != "" {
-			http.Redirect(w, r, url, 302)
-			return
-		} else {
-			http.NotFound(w, r)
-			return
-		}
-	})
-
-	m.Get("/theme/:name/thumbnail", func(r *http.Request, w http.ResponseWriter, params martini.Params) {
-		url := GetThemeThumbnailCached(params["name"])
-		if url != "" {
-			http.Redirect(w, r, url, 301)
-			return
-		} else {
-			http.NotFound(w, r)
-			return
-		}
-	})
-
-	m.Run()
+	r.Run(":" + port)
 }
--- main_test.go
+++ main_test.go
@@ -1,29 +1,98 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetTheme(t *testing.T) {
-	href, found := GetTheme("editor")
-	t.Log(href)
-	if href == "" || !found {
-		t.Fail()
-	}
+	href, found := getTheme("editor")
+	assert := assert.New(t)
+	assert.True(found)
+	assert.NotEmpty(href)
+	assert.Contains(href, "https://")
 }
 
 func TestGetThemeThumbnail(t *testing.T) {
-	href, found := GetThemeThumbnail("editor")
-	t.Log(href)
-	if href == "" || !found {
-		t.Fail()
-	}
+	href, found := getThemeThumbnail("editor")
+	assert := assert.New(t)
+	assert.True(found)
+	assert.NotEmpty(href)
+	assert.Contains(href, "https://")
 }
 
-func TestGetPLugin(t *testing.T) {
-	href, found := GetPlugin("akismet")
-	t.Log(href)
-	if href == "" || !found {
-		t.Fail()
-	}
+func TestGetPlugin(t *testing.T) {
+	href, found := getPlugin("akismet")
+	assert := assert.New(t)
+	assert.True(found)
+	assert.NotEmpty(href)
+	assert.Contains(href, "https://")
+}
+
+func TestGetThemeRequest(t *testing.T) {
+	assert := assert.New(t)
+	ts := GetMainEngine()
+	req, _ := http.NewRequest("GET", "/theme/editor/download", nil)
+	w := httptest.NewRecorder()
+
+	ts.ServeHTTP(w, req)
+
+	assert.Equal(w.Code, 301)
+	assert.Contains(w.Header().Get("Location"), "https://")
+
+}
+
+func TestGetPluginRequestHead(t *testing.T) {
+	assert := assert.New(t)
+	ts := GetMainEngine()
+	req, _ := http.NewRequest("HEAD", "/plugin/akismet/download", nil)
+	w := httptest.NewRecorder()
+
+	ts.ServeHTTP(w, req)
+
+	assert.Equal(w.Code, 301)
+	assert.Contains(w.Header().Get("Location"), "https://")
+
+}
+
+func TestGetThemeRequestHead(t *testing.T) {
+	assert := assert.New(t)
+	ts := GetMainEngine()
+	req, _ := http.NewRequest("HEAD", "/theme/editor/download", nil)
+	w := httptest.NewRecorder()
+
+	ts.ServeHTTP(w, req)
+
+	assert.Equal(w.Code, 301)
+	assert.Contains(w.Header().Get("Location"), "https://")
+
+}
+
+func TestGetPluginRequest(t *testing.T) {
+	assert := assert.New(t)
+	ts := GetMainEngine()
+	req, _ := http.NewRequest("GET", "/plugin/akismet/download", nil)
+	w := httptest.NewRecorder()
+
+	ts.ServeHTTP(w, req)
+
+	assert.Equal(w.Code, 301)
+	assert.Contains(w.Header().Get("Location"), "https://")
+
+}
+
+func TestGetThumbnailRequest(t *testing.T) {
+	assert := assert.New(t)
+	ts := GetMainEngine()
+	req, _ := http.NewRequest("GET", "/theme/editor/thumbnail", nil)
+	w := httptest.NewRecorder()
+
+	ts.ServeHTTP(w, req)
+
+	assert.Equal(w.Code, 301)
+	assert.Contains(w.Header().Get("Location"), "https://")
+
 }
